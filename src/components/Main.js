f1c3780c6ae41eb1b51764822c1629d4d854a699
Zabeeh Khan <zabeehkhan@gmail.com>
2024-01-16 14:57:07 -0800

    top info section completed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,22 @@
+import foodImage from '../assets/restauranfood.jpg';
+
 function Main() {
     return (
         <main>
-            Main
+            <section className="info-section">
+                <div>
+                    <h1 className='text-yellow'>Little Lemon</h1>
+                    <h2 className='text-white'>Chicago</h2>
+                    <p className='text-white lead-text'>
+                        Little Lemon is a culinary oasis nestled in the heart of Chicago, offering a delightful escape into the flavors of the Mediterranean.
+                        Here you are greeted by a warm and inviting atmosphere that reflects the rich cultural tapestry of the region.
+                    </p>
+                    <button className='yellow-button'>Reserve a Table</button>
+                </div>
+                <div className='info-image-div'>
+                    <img src={foodImage} alt="Restaurant Food"></img>
+                </div>
+            </section>
         </main>
     );
 }
