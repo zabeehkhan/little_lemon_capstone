0b92b5a6d749c92853cc0942de1f826b434c8a89
Zabeeh Khan <zabeehkhan@gmail.com>
2024-01-22 14:46:50 -0800

    firebase hosting and github actions setup

---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBfWbabReUIObt69IcoLI-DiWcKagd-zFk",
+    authDomain: "little-lemon-capstone.firebaseapp.com",
+    projectId: "little-lemon-capstone",
+    storageBucket: "little-lemon-capstone.appspot.com",
+    messagingSenderId: "370635529692",
+    appId: "1:370635529692:web:709a05e8de4fc3139ac319",
+    measurementId: "G-K4Q2784X8N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
