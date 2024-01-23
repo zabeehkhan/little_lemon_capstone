7d33e75919e2f82793ea735fed00d72f8b051077
Zabeeh Khan <zabeehkhan@gmail.com>
2024-01-23 11:11:41 -0800

    small clean ups

--- src/pages/ConfirmedBookingPage.js
+++ src/pages/ConfirmedBookingPage.js
@@ -1,8 +1,6 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
-
-import { useEffect } from "react";
 import { useLocation } from 'react-router-dom';
 
 function ConfirmedBookingPage(props) {
